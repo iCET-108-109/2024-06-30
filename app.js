2c9994f7d934d9093f5e32e4ce6e05a8b808015e
icet-sharadha <icet.sharadha@gmail.com>
2024-06-30 15:39:09 +0530

    basics

---
+++ app.js
@@ -0,0 +1,128 @@
+// console.log("hello");
+
+// class Exampple{
+//     public static void main(String [] args){
+
+//     }
+// }
+
+// let sum = run(12,23);
+
+// console.log(sum);
+
+// function run(x, y){
+//     return x+y;
+// }
+
+// let customer = {
+//     name:"saman",
+//     age:12,
+//     address:"gampaha"
+// }
+
+// let customer2 = {
+//     name:"kamal",
+//     age:20,
+//     address:"anuradhapura"
+// }
+
+// console.log(customer);
+// console.log(customer2);
+
+
+// class Customer{
+   
+//     constructor(name,age,address){
+//         this.name=name;
+//         this.age=age;
+//         this.address=address;
+//     }
+//     setName(name) {
+//         this.name=name;
+//     }
+//     getName(){
+//         return this.name;
+//     }
+// }
+
+// let customer= new Customer(125,20,"gampaha");
+
+// let customer2= new Customer("kamal",21,"anuradhapura");
+
+// new Customer();
+
+// console.log(customer.getName());
+// customer2.setName("sumanapala");
+// console.log(customer2);
+
+
+// let numbers = [
+//     10,
+//     "20",
+//     30,
+//     true,
+//     {
+//         name:"saman",
+//         age:20,
+//         address:"gampaha"
+//     },
+//     60
+// ];
+
+// console.log( numbers);
+
+// let customers = [
+//     {
+//         name:"saman",
+//         age:"12"
+//     },
+//     {
+//         name:"kamal",
+//         age:"20"
+//     },
+//     {
+//         name:"vimal",
+//         age:"32"
+//     },
+//     {
+//         name:"nimal",
+//         age:"15"
+//     },
+//     {
+//         name:"sunil",
+//         age:"50",
+//         address:"gampaha"
+//     },
+//     {
+//         name:"ranil",
+//         age:"30"
+//     }
+// ]
+
+// for(let i = 0; i<customers.length; i++){
+//     console.log(customers[i].name +" | "+ customers[i].age);
+//     console.log();
+// }
+
+let numbers = [5,8,9,12,1,7,8,2,3,2]
+
+let words = ["bggdf","fdsf","asdf","rrgb","csdf","dfdg","zxasd","vsd","po","re"]
+
+numbers.push(100);
+numbers.push(200);
+
+numbers.
+
+console.log(numbers.sort());
+
+console.log(words.sort());
+
+
+
+
+
+
+
+
+
+
